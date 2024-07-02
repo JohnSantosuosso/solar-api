3c2d72edb18f51fef7c36852b3edf049e9fb11ef
John Santosuosso <john.santosuosso1@gmail.com>
2024-07-01 22:57:51 -0400

    add additional loggin

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,3 @@
 class Api::V1::BaseController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 end
--- app/controllers/api/v1/external_services_controller.rb
+++ app/controllers/api/v1/external_services_controller.rb
@@ -1,4 +1,7 @@
+require 'net/http'
+
 class Api::V1::ExternalServicesController < Api::V1::BaseController
+  before_action :log_headers
   before_action :authenticate_user!
 
   def fetch_data
@@ -8,5 +11,16 @@ class Api::V1::ExternalServicesController < Api::V1::BaseController
     # Perform your API call or other actions here
     uri = URI('https://random.dog/woof.json')
     response = Net::HTTP.get(uri)
+
+    render json: response
+  end
+
+  private
+  def log_headers
+    authorization_header = request.headers['Authorization']
+    File.open('log/headers.log', 'a') do |f|
+      f.puts "Headers: #{request.headers.inspect} #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
+      f.puts "Authorization Header: #{authorization_header}"
+    end
   end
 end
